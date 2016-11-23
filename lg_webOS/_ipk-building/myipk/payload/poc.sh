b94553c2d8f0ef5c903f9dbcbd934dec725124d7
Conor Horan-Kates <conor.code@gmail.com>
2016-11-23 15:42:51 -0800

    committing ipk sources

---
+++ lg_webOS/_ipk-building/myipk/payload/poc.sh
@@ -0,0 +1,3 @@
+#!/bin/ash
+# ugh, not sure i should be specifying that
+# nc -l -vvv localhost 1234 # open-wrt does not support -l
--- lg_webOS/myipk/payload/poc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/ash
-# ugh, not sure i should be specifying that
-nc -l -vvv localhost 1234
